00747a3f311def4ae784007a17c7f6ada7da7408
Washim Ahmed <washim.ahmed@gmail.com>
2014-12-13 21:06:01 +0300

    Automated Push in Master Branch By Cron

--- auto-git-commit.sh
+++ auto-git-commit.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-cd /home/tmwgroups/www/db_backup
-drush sql-dump > default.sql
-drush sql-dump --uri=adibasi > adibasi.sql
-drush sql-dump --uri=washimahmed > washimahmed.sql
-drush sql-dump --uri=demo > demo.sql
-cd ../
+cd /home/tmwgroups/www
+drush sql-dump > db_backup/default.sql
+drush sql-dump --uri=adibasi > db_backup/adibasi.sql
+drush sql-dump --uri=washimahmed > db_backup/washimahmed.sql
+drush sql-dump --uri=demo > db_backup/demo.sql
 git add ./
 git commit -m 'Automated Push in Master Branch By Cron'
 git push origin master
-echo "Automated Push in Master Branch By Cron"
 
