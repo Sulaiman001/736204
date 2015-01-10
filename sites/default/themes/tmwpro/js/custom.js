dc7de94fc2eaaef437f1306c4da49af4eef13a16
Washim Ahmed <washim.ahmed@gmail.com>
2015-01-11 00:01:00 +0300

    Scripted auto-commit on Sun Jan 11 00:00:01 MSK 2015 by Shell

--- sites/default/themes/tmwpro/js/custom.js
+++ sites/default/themes/tmwpro/js/custom.js
@@ -1,7 +1,7 @@
 (function ($) {
 Drupal.behaviors.myModuleSecureLink = {
   attach: function (context, settings) {
-		$('.page-user .region-content,.page-newsletter .region-content').addClass('container');
+	$('.page-user .region-content,.page-newsletter .region-content,.page-search .region-content').addClass('container');
     $('#user-login,#user-pass,#user-register-form,.block-newsletter,#newsletter-manage-subscriptions-form').addClass('tmwbox col-md-7');
   }
 };
